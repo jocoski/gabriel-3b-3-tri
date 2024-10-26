8ef78a636bb75e274ffec3bddfd8f8a56d98d5ce
jocoski <132354583+jocoski@users.noreply.github.com>
2024-10-25 22:38:46 -0300

    Add files via upload

--- gabriel-3b-3-tri-main/graficos/QuantidadeUsuario.js
+++ gabriel-3b-3-tri-main/graficos/QuantidadeUsuario.js
@@ -1,3 +1,6 @@
+import { getCSS } from "./common.js";
+
+
 async function quantidadeUsuariosPorRede() {
   const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
   const res = await fetch(url)
@@ -6,17 +9,25 @@ async function quantidadeUsuariosPorRede() {
   const quantidadeDeUsuarios = Object.values(dados)
 
   const data = [
-      {
-          x: nomeDasRedes, 
-          y: quantidadeDeUsuarios, 
-          type: 'bar'
+    {
+      x: 'nomeDasRedes',
+      y: quantidadeDeUsuarios,
+      type: 'bar',
+      marker: {
+        color: getCSS('--primary-color')
       }
+    }
   ]
+  
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color')
+}
 
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
+  Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuariosPorRede()
